98fbb50b71e99407f1102639b61bf22e8b84f8b1
Henry-Ning <henryhuning@gmail.com>
2020-07-01 13:51:02 -0700

    25. [Python] - Objects - Understanding Attributes and Methods

--- life.py
+++ life.py
@@ -1,1 +1,12 @@
-theAnswer = 42
+import math
+
+theAnswer = 44
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+    
+def square_root(a):
+    return a**0.5
+
+def sq_rt(a):
+    return math.sqrt(a)
